71bf7808116cda9d8645188b407389e6aa435a60
Shaheer Amjad <shaheerszm@gmail.com>
2022-10-17 19:36:37 -0400

    Clear and delete buttons

--- index.js
+++ index.js
@@ -2,16 +2,14 @@
 class Calculator {
   operation;
   value;
-
-  constructor(){
-    this.value = 0.0;
+  constructor(val = 0.0){
+    this.value = val;
     this.operation = null;
   }
-
   oper(arg){
+    if (arg == 'null') { this.operation = null; }
     this.operation = String(arg);
   }
-
   update(arg){
     if (this.operation == null){
       this.value = Number(arg);
@@ -20,11 +18,9 @@ class Calculator {
     this.value = this.calculate(this.value, Number(arg), this.operation)
     }
   }
-
   get value(){
     return this.value
   }
-
   calculate(a, b, operation){
     switch(operation){
       case 'add':
@@ -55,21 +51,48 @@ for (let item of btns) {
   item.addEventListener('click', clickNum);
   }
 }
-
+function updateDisplay(val) {
+  dispVal = 0;
+  if (String(val).length > 10){
+    display.textContent = Math.round((val + Number.EPSILON) * 1e8) / 1e8;
+  }
+  else {
+    display.textContent = val;
+  }    
+}
 function clickNum(e) {
   let val = e.target.value;
+  if (String(dispVal).length > 8 && dispVal != 0) {return}
   dispVal *= 10;
   dispVal += Number(val)
   display.textContent = dispVal;
 }
-
-
 function clickOper(e) {
   let oper = e.target.value;
+  calc.update(dispVal);
   calc.oper(oper);
-  document.querySelectorAll('.display').innerText = calc.value;
+  updateDisplay(calc.value);
 }
 function clickOption(e) {
   let opt = e.target.value;
-  console.log(opt)
+  if (opt == "Clear") {
+    calc = new Calculator();
+    updateDisplay(0);
+  }
+  if (opt == "Equals") {
+    if (calc.operation == 'null') { return; }
+    calc.update(dispVal);
+    updateDisplay(calc.value);
+    dispVal = calc.value;
+    calc = new Calculator(calc.value);
+  }
+  if (opt == "Delete") {
+    dispVal -= dispVal % 10;
+    dispVal /= 10;
+    updateDisplay(dispVal);
+  }
+  if (opt == "+/-") {
+    dispVal *= -1;
+    updateDisplay(dispVal);
+  }
 }
